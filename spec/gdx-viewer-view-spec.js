53aca2a317455d61d725b1ac065d566b5f8f1103
Christoph Pahmeyer <christoph.pahmeyer@ilr.uni-bonn.de>
2019-08-28 11:11:15 +0200

    init

---
+++ spec/gdx-viewer-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GdxViewerView from '../lib/gdx-viewer-view';
+
+describe('GdxViewerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
